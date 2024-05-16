8b15d7122ce91f893c50df00f1f0e904f8d4b436
asannasa2006 <biaanna104@gmail.com>
2024-05-16 11:40:10 -0300

    Feat: aula 1
    Aprendi como printar strins, declarar variáveis e printa-las em conjunto de strings.

---
+++ aula1.py
@@ -0,0 +1,26 @@
+#Aula 1
+
+print ("Hello World!")
+print ( "Brasil ganhou 5 copas do mundo")
+print ("Brasil", "ganhou", 5,"copas do mundo" , sep="-")
+# o sep define oque tera entre os valores definidos 
+# sep="-" printara - a cada valor
+print ("Brasil", "ganhou", 5,"copas do mundo" , end=".\n")
+# o end printa ao final dos valores.
+
+pais = "Italia"
+#para atribuir variavel basta adicionar o nome = valor
+
+type(pais)
+#questinar qual o tipo de variavel foi atribuida 
+#"CLASS STR (string)"
+
+quantidade = 4
+type(quantidade)
+#"CLASS INT (inteiro)"
+
+print (pais, "ganhou", quantidade, "títulos mundiais.") 
+#printar valores basta colocar o nome dado a variavel
+# pode printar strings sem declararas como variaveis
+
+
