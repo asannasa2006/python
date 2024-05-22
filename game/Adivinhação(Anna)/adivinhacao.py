26d9c7894277bdf520ca4fdeb23a900b7ffee50a
asannasa2006 <biaanna104@gmail.com>
2024-05-22 12:38:55 -0300

    ELIF

--- game/Adivinhação(Anna)/adivinhacao.py
+++ game/Adivinhação(Anna)/adivinhacao.py
@@ -1,19 +1,26 @@
 print("**************************************")
-print("Olá. Bem vindo ao jogo de Adivinhação!")
+print("Olá. Bem-vindo ao jogo de Adivinhação!")
 print("**************************************")
 
 numero_secreto1 = 42
 
-chute = input("Digite um número: ",)
-chute = int(chute)
+chute_str = input("Digite um número: ",) #dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int.
+chute_str = int(chute_str) #converção de string para int.
 
-print( "Você propos o numero: ", chute )
+print( "Você propos o numero: ", chute_str )
 
-if (numero_secreto1 == chute):
-    print("E você acerto, Parabéns!")
-#para atribuir condições dentro do if/else é necessário ter 4 espaços ou 1 tab
+#if (numero_secreto1 == chute_str):
+#    print("E você acerto, Parabéns!")
+#else:
+#    if (chute_str > numero_secreto1):
+#        print( "E você errou. O número proposto é maior que o número secreto.") #para atribuir condições dentro do if/else é necessário ter 4 espaços ou 1 tab.
+#    elif (chute_str < numero_secreto1): # o else pode ser usada apenas uma ve dentro do bloco.
+#        print( "E você errou. Seu número é menor que o número secreto")
 
-else:
-    print( "E você errou, tente novamente.")
+# tudo acima funcionará, mas não será agil)
 
-#dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int
+acertou= chute_str == numero_secreto1
+
+
+
+print ("\nFim de jogo ************") # \n é um comando para pular linha.
