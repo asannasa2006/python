b4602e6926f28ddf675f9257c7d911f8c7c30364
asannasa2006 <biaanna104@gmail.com>
2024-06-11 09:30:44 -0300

    Atualizações

--- game/Adivinhação(Anna)/adivinhacao.py
+++ game/Adivinhação(Anna)/adivinhacao.py
@@ -3,24 +3,30 @@ print("Olá. Bem-vindo ao jogo de Adivinhação!")
 print("**************************************")
 
 numero_secreto1 = 42
-
-chute_str = input("Digite um número: ",) #dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int.
-chute_str = int(chute_str) #converção de string para int.
-
-print( "Você propos o numero: ", chute_str )
-
-#if (numero_secreto1 == chute_str):
-#    print("E você acerto, Parabéns!")
-#else:
-#    if (chute_str > numero_secreto1):
-#        print( "E você errou. O número proposto é maior que o número secreto.") #para atribuir condições dentro do if/else é necessário ter 4 espaços ou 1 tab.
-#    elif (chute_str < numero_secreto1): # o else pode ser usada apenas uma ve dentro do bloco.
-#        print( "E você errou. Seu número é menor que o número secreto")
-
-# tudo acima funcionará, mas não será agil)
-
-acertou= chute_str == numero_secreto1
+total_de_tentativas= 3
+rodada = 1
 
 
+while (rodada <= total_de_tentativas):
+    print("\nTentativas:",  total_de_tentativas)
+    chute_str = input("\nDigite um número: ",) #dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int.
+    chute_str = int(chute_str) #converção de string para int.
 
+    print( "\nVocê propos o numero: ", chute_str )
+
+    acertou = chute_str == numero_secreto1
+    maior = chute_str > numero_secreto1
+    menor = chute_str < numero_secreto1
+
+    if(acertou):
+        print("Parabéns! Você acertou!")
+    else:
+        if(maior):
+            print("O seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("O seu chute foi menor do que o número secreto!")
+
+    total_de_tentativas = total_de_tentativas - 1
+
+print("\nSuas tentativas acabou!")
 print ("\nFim de jogo ************") # \n é um comando para pular linha.
