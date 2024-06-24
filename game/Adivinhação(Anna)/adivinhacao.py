b77a7483b047b27660805d3dd58146bc603bf2f8
asannasa2006 <biaanna104@gmail.com>
2024-06-24 09:02:08 -0300

    24/06

--- game/Adivinhação(Anna)/adivinhacao.py
+++ game/Adivinhação(Anna)/adivinhacao.py
@@ -1,32 +1,57 @@
-print("**************************************")
-print("Olá. Bem-vindo ao jogo de Adivinhação!")
-print("**************************************")
+import random
 
-numero_secreto1 = 42
-total_de_tentativas= 3
-rodada = 1
+def jogar():
+    print("**************************************")
+    print("Olá. Bem-vindo ao jogo de Adivinhação!")
+    print("**************************************")
 
+    numero_secreto1 = random.randrange(1, 101)
+    total_de_tentativas= 0
+    pontos= 100
 
-while (rodada <= total_de_tentativas):
-    print("\nTentativas:",  total_de_tentativas)
-    chute_str = input("\nDigite um número: ",) #dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int.
-    chute_str = int(chute_str) #converção de string para int.
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-    print( "\nVocê propos o numero: ", chute_str )
+    nivel = int(input("Defina o nível: "))
 
-    acertou = chute_str == numero_secreto1
-    maior = chute_str > numero_secreto1
-    menor = chute_str < numero_secreto1
-
-    if(acertou):
-        print("Parabéns! Você acertou!")
+    if (nivel == 1):
+        total_de_tentativas = 20
+    elif (nivel == 2):
+        total_de_tentativas = 10
     else:
-        if(maior):
-            print("O seu chute foi maior do que o número secreto!")
-        elif(menor):
-            print("O seu chute foi menor do que o número secreto!")
+        total_de_tentativas = 5
 
-    total_de_tentativas = total_de_tentativas - 1
+    for rodada in range(1, total_de_tentativas + 1):
+        print("\nTentativa {} de {}".format(rodada, total_de_tentativas))
+        chute_str = input("\nDigite um número entre 1 e 100: ",) #dara um erro, pois ao chute é tipo string (por causa do input) e o numero_screto1 é int.
+        chute_str = int(chute_str) #converção de string para int.
 
-print("\nSuas tentativas acabou!")
-print ("\nFim de jogo ************") # \n é um comando para pular linha.
+        print( "\nVocê propos o numero: ", chute_str )
+
+        if (chute_str < 1 or chute_str > 100):
+            print("Você deve digitar um número entre 1 e 100!")
+            continue
+
+        acertou = chute_str == numero_secreto1
+        maior = chute_str > numero_secreto1
+        menor = chute_str < numero_secreto1
+
+        if(acertou):
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
+        else:
+            if(maior):
+                print("O seu chute foi maior do que o número secreto!")
+            elif(menor):
+                print("O seu chute foi menor do que o número secreto!")
+            pontos_perdidos = abs(numero_secreto1 - chute_str)
+            pontos = pontos - pontos_perdidos
+
+
+        total_de_tentativas = total_de_tentativas - 1
+
+    print("\nSuas tentativas acabou!")
+    print ("\nFim de jogo ************") # \n é um comando para pular linha.
+
+if (__name__ == "__main__"):
+    jogar()
